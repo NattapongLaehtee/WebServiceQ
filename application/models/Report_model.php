f32afe3f4979c004495de420f9a49c483fe6d9cb
ASUSWiTT <ASUSWiTT@172.16.184.74>
2018-09-18 17:12:52 +0700

    edit report

--- application/models/Report_model.php
+++ application/models/Report_model.php
@@ -3,12 +3,10 @@ class Report_model extends CI_Model{
     
     public function student_report() {
         
-        $this->db->select('Studentid, Studentname, Studentsurname, Facultyname ');
-        $this->db->from('student, faculty');
-        $this->db-join();
+      
         $query = $this->db->query('select Studentid, Studentname, Studentsurname, Facultyname 
                                     from student, faculty
-                                    where student.Studentid =  faculty.Facultyid');
+                                    where student.Studentid join faculty.Facultyid');
            return $query->result();                          
      
     }
